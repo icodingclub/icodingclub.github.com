3f4a35efb070544f34a60df8e296289b8dab6a83
icodingclub <mails.icodingclub@gmail.com>
2014-10-11 11:19:39 +0530

    updated

--- blogger/js/topics.js
+++ blogger/js/topics.js
@@ -4,17 +4,17 @@ var articleCatagorys = [{
         articles: [{
             heading: "Dojo Toolkit Tutorial (AMD): Hello world application in DOJO and AMD foundation apis.",
             desc: "A step-by-step tutorial for Hello World application or tutorial using new DOJO AMD(Asynchronous Module Definition) concept.<br>This article also touch the AMD foundation API like require() and define() as well as DOJO api declare()",
-            link: "/dojo/hello-world-by-dojo-amd.html",
+            link: "http://icodingclub.github.io/dojo/hello-world-by-dojo-amd.html",
             image: "dojo.png"
         }, {
             heading: "Dojo Toolkit Tutorial (AMD): Inheritance or MixIn in DOJO using declare and AMD concept",
             desc: "A step-by-step tutorial for, How Inheritance or Mixin works using DOJO declare()",
-            link: "/dojo/dojo-amd-declare-inheritance.html",
+            link: "http://icodingclub.github.io/dojo/dojo-amd-declare-inheritance.html",
             image: "dojo.png"
         }, {
             heading: "Dojo Toolkit Tutorial (AMD): Simple template based widget using AMD concepts",
             desc: "A step-by-step tutorial for, How to create a template based DOJO widget, using AMD(Asynchronous Module Definition) concepts",
-            link: "/dojo/dojo-amd-template-based-widget.html",
+            link: "http://icodingclub.github.io/dojo/dojo-amd-template-based-widget.html",
             image: "dojo.png"
         }]
     }, {
@@ -23,12 +23,12 @@ var articleCatagorys = [{
         articles: [{
             heading: "Pass by Value vs Pass by reference: Javascript and Java....and C",
             desc: "This article explanation, what happen when you create an Object and reference in Javascript or Java.How pass by value vs pass by reference in Javascript and Java work.Its take you back in History in C language world and explain how's this concept started and evolved.",
-            link: "/passByValVsRef/Pass-By-Value-Vs-pass-by-reference.html",
+            link: "http://icodingclub.github.io/passByValVsRef/Pass-By-Value-Vs-pass-by-reference.html",
             image: "passByValRef.png"
         }, {
             heading: "HTML5 AJAX File upload (Image or HTML5 Canvas data) to Java backend",
             desc: "This article explains how you can upload or read an image file from your local file system, display it as image and HTML5 Canvas and upload it to java backend.I am using new HTML5 api for multipart upload or FormData upload and java7 api for MultipartConfig upload.",
-            link: "/fileupload/file-upload-ajax.html",
+            link: "http://icodingclub.github.io/fileupload/file-upload-ajax.html",
             image: "image-upload.png"
         }, {
             heading: "Tiles 3 with Spring 3 with JSP or FTL view",
@@ -42,22 +42,22 @@ var articleCatagorys = [{
         articles: [{
             heading: "HTML5 Canvas Image cropper: Flexible selection approch",
             desc: "Let you crop image with flexibility to select any area on image by dragging the mouse on image. I have built it with pure Javascript and HTML5 apis. To upload file on server Java has been used.",
-            link: "/imagecropper/imagecropper-flex-sel.html",
+            link: "http://icodingclub.github.io/imagecropper/imagecropper-flex-sel.html",
             image: "image_flex_crop.png"
         }, {
             heading: "HTML5 Canvas Image cropper: Fixed selection approch",
             desc: "Let you crop image, but with restriction of aspect ratio. You can pan and zoom the image as you need before cropping.I have built it with pure Javascript and HTML5 apis. To upload file on server Java has been used.",
-            link: "/imagecropper/imagecropper-fixed-sel.html",
+            link: "http://icodingclub.github.io/imagecropper/imagecropper-fixed-sel.html",
             image: "image_crop_fixed.png"
         }, {
             heading: "Simple tree widget using JQuery",
             desc: "A very simple tree widget, made out of JQuery.",
-            link: "/treewidget/treewidget.html",
+            link: "http://icodingclub.github.io/treewidget/treewidget.html",
             image: "tree-widget.png"
         }, {
             heading: "Accordion widget using JQuery",
             desc: "A very simple Accordion widget built out of Javascript and JQuery",
-            link: "/accordionwidget/accordion-single-pane.html",
+            link: "http://icodingclub.github.io/accordionwidget/accordion-single-pane.html",
             image: "accodion-pane.png"
         }]
     }, {
