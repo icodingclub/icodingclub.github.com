bbd7ed8c78c886dc17f1f2e3c2ae3077796e5947
icodingclub <mails.icodingclub@gmail.com>
2014-10-11 11:06:28 +0530

    updated

---
+++ blogger/js/base.js
@@ -0,0 +1,30 @@
+var articleBuilder = {
+    articleCatagoryTemplate: document.getElementById("articleCatagoryTemplate").innerHTML,
+
+    articleTemplate: document.getElementById("articleTemplate").innerHTML,
+    buildAllArticle: function() {
+        var i, j, articleHtml = "",
+            articleCatagoryHtml = "",
+            articleTemplate, articleCatagoryTemplate, article, articleCatagory;
+
+
+        for (i = 0; i < articleCatagorys.length; i++) {
+            articleCatagory = articleCatagorys[i];
+            articleHtml = "";
+            for (j = 0; j < articleCatagory.articles.length; j++) {
+                article = articleCatagory.articles[j];
+                articleTemplate = this.articleTemplate;
+                articleTemplate = articleTemplate.replace("{{iamge}}", "/resource/image/" + article.image);
+                articleTemplate = articleTemplate.replace("{{heading}}", article.heading);
+                articleTemplate = articleTemplate.replace("{{desc}}", article.desc);
+                articleTemplate = articleTemplate.replace("{{link}}", article.link).replace("{{link}}", article.link).replace("{{link}}", article.link);
+                articleHtml += articleTemplate;
+            }
+            articleCatagoryTemplate = this.articleCatagoryTemplate;
+            articleCatagoryTemplate = articleCatagoryTemplate.replace("{{articleSectionHeading}}", articleCatagory.displayName);
+            articleCatagoryTemplate = articleCatagoryTemplate.replace("{{articles}}", articleHtml);
+            articleCatagoryHtml += articleCatagoryTemplate;
+        }
+        document.getElementById("articlesContainer").innerHTML = articleCatagoryHtml;
+    }
+};
