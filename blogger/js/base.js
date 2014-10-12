3c56f38d28e2704d4dff1015223a5a506741516e
icodingclub <mails.icodingclub@gmail.com>
2014-10-12 09:05:24 +0530

    updated

--- blogger/js/base.js
+++ blogger/js/base.js
@@ -1,7 +1,26 @@
 var articleBuilder = {
-    articleCatagoryTemplate: document.getElementById("articleCatagoryTemplate").innerHTML,
+    articleCatagoryTemplate: "<h1 class='section-heading'>{{articleSectionHeading}}" +
+        "<span class='arrow'></span>" +
+        "</h1>" +
+        "<br>" +
+        "<section class='articleSection'>{{articles}}</section>",
 
-    articleTemplate: document.getElementById("articleTemplate").innerHTML,
+    articleTemplate: "<article>" +
+        "<div class='articleImg'>" +
+        "<a href='{{link}}'>" +
+        "<img src='{{iamge}}'>" +
+        "</a>" +
+        "</div>" +
+        "<div class='articleBody'>" +
+        "<a href='{{link}}'>" +
+        "<h1>{{heading}}</h1>" +
+        "</a>" +
+        "<p class='desc'>{{desc}}</p>" +
+        "<a href='{{link}}'>" +
+        "<span class='readMore'>tutorial</span>" +
+        "</a>" +
+        "</div>" +
+        "</article>",
     buildAllArticle: function() {
         var i, j, articleHtml = "",
             articleCatagoryHtml = "",
@@ -26,16 +45,37 @@ var articleBuilder = {
             articleCatagoryHtml += articleCatagoryTemplate;
         }
         document.getElementById("articlesContainer").innerHTML = articleCatagoryHtml;
-    }
-};
+    },
+
+    getArticlesByTag: function(tagName, heading) {
+        var i, j, articleHtml = "",
+            articleTemplate, articleCatagoryTemplate, article, articleCatagory;
+
+        articleCatagoryTemplate = this.articleCatagoryTemplate;
+        articleCatagoryTemplate = articleCatagoryTemplate.replace("{{articleSectionHeading}}", heading);
+
+        for (i = 0; i < articleCatagorys.length; i++) {
+            articleCatagory = articleCatagorys[i];
+            for (j = 0; j < articleCatagory.articles.length; j++) {
+                article = articleCatagory.articles[j];
+                if (article.tag.indexOf(tagName) > -1) {
+                    articleTemplate = this.articleTemplate;
+                    articleTemplate = articleTemplate.replace("{{iamge}}", "http://icodingclub.github.io/resource/image/" + article.image);
+                    articleTemplate = articleTemplate.replace("{{heading}}", article.heading);
+                    articleTemplate = articleTemplate.replace("{{desc}}", article.desc);
+                    articleTemplate = articleTemplate.replace("{{link}}", article.link).replace("{{link}}", article.link).replace("{{link}}", article.link);
+                    articleHtml += articleTemplate;
+                }
+
+            }
 
 
-var codePrettify = {
-    init: function() {
-        var body = document.getElementsByTagName('body')[0];
-        var script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.src = 'https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js?lang=css&skin=sunburst';
-        body.appendChild(script);
+
+        }
+
+        articleCatagoryTemplate = articleCatagoryTemplate.replace("{{articles}}", articleHtml);
+
+
+        document.getElementById("articlesContainer").innerHTML = articleCatagoryTemplate;
     }
 };
