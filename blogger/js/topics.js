04c4ce67d2c03b9e63fea5965ba8ca7a8b6716f4
icodingclub <mails.icodingclub@gmail.com>
2015-01-31 07:12:57 +0530

    done

--- blogger/js/topics.js
+++ blogger/js/topics.js
@@ -6,55 +6,55 @@ var articleCatagorys = [{
             desc: "This article explains how you can upload or read an image file from your local file system, display it as image and HTML5 Canvas and upload it to java backend.I am using new HTML5 api for multipart upload or FormData upload and java7 api for MultipartConfig upload.",
             link: "http://icodingclub.blogspot.in/2014/10/html5-ajax-file-upload-image-or-html5.html",
             image: "image-upload.png",
-            tag: "webui java html5"
+            tag: "all webui java html5"
         }, {
             heading: "Simple Inheritance in Javascript",
             desc: "A step by step guide for Javascript prototypes based Inheritance.",
             link: "http://icodingclub.blogspot.in/2014/12/simple-inheritance-in-javascript.html",
             image: "javascript_inheritance_class_diagram-logo.png",
-            tag: "webui"
+            tag: "all webui"
         },{
             heading: "Pass by Value vs Pass by reference: Javascript and Java....and C",
             desc: "This article explanation, what happen when you create an Object and reference in Javascript or Java.How pass by value vs pass by reference in Javascript and Java work.Its take you back in History in C language world and explain how's this concept started and evolved.",
             link: "http://icodingclub.blogspot.in/2014/10/pass-by-value-vs-pass-by-reference.html",
             image: "passByValRef.png",
-            tag: "webui java"
+            tag: "all webui java"
         }, {
             heading: "Tiles 3 with Spring 3 with JSP or FTL view",
             desc: "Step by step tutorial of tiles3 web application using Spring3. I have build it with JSP and FTL view.",
             link: "http://icodingclub.blogspot.in/2013/12/tutorial-tiles-3-with-spring-3-with-jsp.html",
             image: "tiles3.png",
-            tag: "webui java"
+            tag: "all webui java"
         }, {
             heading: "HTML5 Canvas Image cropper: Flexible selection approch",
             desc: "Let you crop image with flexibility to select any area on image by dragging the mouse on image. I have built it with pure Javascript and HTML5 apis. To upload file on server Java has been used.",
             link: "http://icodingclub.blogspot.in/2014/10/html5-canvas-image-cropper-flexible.html",
             image: "image_flex_crop.png",
-            tag: "webui html5"
+            tag: "all webui html5"
         }, {
             heading: "HTML5 Canvas Image cropper: Fixed selection approch",
             desc: "Let you crop image, but with restriction of aspect ratio. You can pan and zoom the image as you need before cropping.I have built it with pure Javascript and HTML5 apis. To upload file on server Java has been used.",
             link: "http://icodingclub.blogspot.in/2014/10/introduction-html5-canvas-image-cropper.html",
             image: "image_crop_fixed.png",
-            tag: "webui html5"
+            tag: "all webui html5"
         }, {
             heading: "AJAX Framework using JQuery: An Object Oriented approach",
             desc: "A very simple AJAX Framework using JQuery for light weight application, with an object oriented approach.",
             link: "http://icodingclub.blogspot.in/2013/07/object-oriented-ajax-framework-using.html",
             image: "jquery.png",
-            tag: "webui"
+            tag: "all webui"
         }, {
             heading: "Simple tree widget using JQuery",
             desc: "A very simple tree widget, made out of JQuery.",
             link: "http://icodingclub.blogspot.in/2013/10/simplest-approach-tree-widget.html",
             image: "tree-widget.png",
-            tag: "webui"
+            tag: "all webui"
         }, {
             heading: "Accordion widget using JQuery",
             desc: "A very simple Accordion widget built out of Javascript and JQuery",
             link: "http://icodingclub.blogspot.in/2013/10/simplest-approach-accordion-widget.html",
             image: "accodion-pane.png",
-            tag: "webui"
+            tag: "all webui"
         }
     ]
 }, {
@@ -65,25 +65,25 @@ var articleCatagorys = [{
         desc: "A step-by-step tutorial for Hello World application or tutorial using new DOJO AMD(Asynchronous Module Definition) concept.<br>This article also touch the AMD foundation API like require() and define() as well as DOJO api declare()",
         link: "http://icodingclub.blogspot.in/2014/10/dojo-toolkit-tutorial-amd-hello-world.html",
         image: "dojo.png",
-        tag: "dojo"
+        tag: "all dojo"
     }, {
         heading: "Simple template based widget using AMD concepts",
         desc: "A step-by-step tutorial for, How to create a template based DOJO widget, using AMD(Asynchronous Module Definition) concepts",
         link: "http://icodingclub.blogspot.in/2014/10/dojo-toolkit-tutorial-amd-simple.html",
         image: "dojo.png",
-        tag: "dojo"
+        tag: "all dojo"
     }, {
         heading: "JAVA LIKE INHERITANCE IN JAVASCRIPT USING DOJO",
         desc: "A step-by-step tutorial for, How Dojo provide Java like Inheritance, very different then traditional Javascript prototype based inheritance.",
         link: "http://icodingclub.blogspot.in/2014/12/dojo-toolkit-tutorial-amd-java-like.html",
         image: "dojo.png",
-        tag: "dojo"
+        tag: "all dojo"
     }, {
         heading: "Deep dive into MixIn based Inheritance in DOJO using declare and AMD concept",
         desc: "A step-by-step tutorial for, How Inheritance or Mixin works using DOJO declare()",
         link: "http://icodingclub.blogspot.in/2014/10/dojo-toolkit-tutorial-amd-inheritance.html",
         image: "dojo.png",
-        tag: "dojo"
+        tag: "all dojo"
     }]
 }, {
     type: "javaArticle",
@@ -93,36 +93,36 @@ var articleCatagorys = [{
         desc: "Step by step tutorial of JMS with Spring 3 and ActiveMQ, in point to point model.",
         link: "http://icodingclub.blogspot.in/2011/07/introduction-of-spring-jms-with.html",
         image: "jms_activemq.png",
-        tag: "java"
+        tag: "all java"
     }, {
         heading: "Spring JMS with Apache ActiveMQ in embedded mode in tomcat",
         desc: "Step by step tutorial of JMS with Spring 3 and ActiveMQ, in embeded model model.",
         link: "http://icodingclub.blogspot.in/2011/09/spring-jms-with-embeded-activemq-in.html",
         image: "jms_activemq.png",
-        tag: "java"
+        tag: "all java"
     }, {
         heading: "Spring JMS with Jboss HornetQ in embedded mode in tomcat",
         desc: "Step by step tutorial of JMS with Spring 3 and HornetQ, in embeded model model.",
         link: "http://icodingclub.blogspot.in/2011/09/spring-jms-with-embeded-hornetq-in.html",
         image: "jms_activemq.png",
-        tag: "java"
+        tag: "all java"
     }, {
         heading: "Using JaxB/Jackson: POJO &lt; - &gt; XML &lt; - &gt; JSON",
         desc: "Step by step tutorial of how to conver a java pojo to XML or JSON.",
         link: "http://icodingclub.blogspot.in/2011/04/pojo-xml-json.html",
         image: "pojo_xml_json.png",
-        tag: "java"
+        tag: "all java"
     }, {
         heading: "Hibernate Search: Google like search in Java",
         desc: "Step by step tutorial of Google like search in java using Hibernate search api.",
         link: "http://icodingclub.blogspot.in/2011/03/google-like-search-in-java-hibernate.html",
         image: "hiber-serach.png",
-        tag: "java"
+        tag: "all java"
     }, {
         heading: "Introducing EhCache",
         desc: "Step by step tutorial of EHCache.",
         link: "http://icodingclub.blogspot.in/2010/10/introducing-ehcache.html",
         image: "ehcache.png",
-        tag: "java"
+        tag: "all java"
     }]
 }];
