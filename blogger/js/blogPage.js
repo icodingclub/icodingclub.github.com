33f38b468367c08052361fc4b6aad91b308584fb
icodingclub <mails.icodingclub@gmail.com>
2015-01-31 12:57:56 +0530

    done

--- blogger/js/blogPage.js
+++
@@ -1,5 +0,0 @@
-$("pre.encodeCodeblock").text($("pre.encodeCodeblock").html());
-$('<script/>', {
-    type:    'text/javascript',
-    src:     'https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js?skin=sunburst'
-}).appendTo(document.body);
