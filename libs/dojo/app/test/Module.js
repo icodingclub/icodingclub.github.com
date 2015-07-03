ee5b7fc19dcfc8673497537b87a4b4d6ac0c0c03
icodingclub <mails.icodingclub@gmail.com>
2015-07-04 00:06:02 +0530

    d

---
+++ libs/dojo/app/test/Module.js
@@ -0,0 +1,14 @@
+define(function() {
+	var privateValue = 0;
+	return {
+		increment : function() {
+			privateValue++;
+		},
+		decrement : function() {
+			privateValue--;
+		},
+		getValue : function() {
+			return privateValue;
+		}
+	};
+});
