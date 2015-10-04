1d2f5e179a9d019af41c78a72319b6d7119ed76f
icodingclub <mails.icodingclub@gmail.com>
2015-10-04 14:24:55 +0530

    d

--- app/js/templates.js
+++ app/js/templates.js
@@ -3,27 +3,27 @@ this["JST"] = this["JST"] || {};
 this["JST"]["article"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
     var stack1, helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
 
-  return "    <section class=\"article\">\n\n        <a href=\""
+  return "    <section class=\"article\">\n        <a href=\""
     + alias3(((helper = (helper = helpers.link || (depth0 != null ? depth0.link : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"link","hash":{},"data":data}) : helper)))
     + "\"><h1>"
     + alias3(((helper = (helper = helpers.heading || (depth0 != null ? depth0.heading : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"heading","hash":{},"data":data}) : helper)))
-    + "</h1></a>\n          <div class=\"article-img hideOnMobile\">\n                <img class=\"hideOnMobile\" src=\"http://icodingclub.github.io/resource/image/"
+    + "</h1></a>\n        <div class=\"table\">\n            <div class=\"tableRow\">\n                <div class=\"tableCell article-img hideOnMobile\">\n                    <img class=\"hideOnMobile\" src=\"http://icodingclub.github.io/resource/image/"
     + alias3(((helper = (helper = helpers.image || (depth0 != null ? depth0.image : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"image","hash":{},"data":data}) : helper)))
     + "\" alt=\""
     + alias3(((helper = (helper = helpers.image || (depth0 != null ? depth0.image : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"image","hash":{},"data":data}) : helper)))
-    + "\">\n             </div>\n\n        <p>\n          "
+    + "\">\n                </div>\n                <div class=\"tableCell\">\n                    <p>\n                        "
     + alias3(((helper = (helper = helpers.desc || (depth0 != null ? depth0.desc : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"desc","hash":{},"data":data}) : helper)))
-    + "\n\n             </p>\n        <div class=\"tags\">\n"
+    + "\n                    </p>\n                    <div class=\"tags\">\n"
     + ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.tags : depth0),{"name":"each","hash":{},"fn":this.program(2, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "")
-    + "\n        </div>\n        <div class='clear-fix'></div>\n\n    </section>\n\n";
+    + "\n                    </div>\n                </div>\n            </div>\n        </div>\n    </section>\n";
 },"2":function(depth0,helpers,partials,data) {
-    return "            <span>"
+    return "                        <span>"
     + this.escapeExpression(this.lambda(depth0, depth0))
     + "</span> ";
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var stack1;
 
-  return "<section class='articleSection'>\n\n"
+  return "<article class='articleSection'>\n"
     + ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.articles : depth0),{"name":"each","hash":{},"fn":this.program(1, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "")
-    + "\n</section>\n";
+    + "</article>\n";
 },"useData":true});
