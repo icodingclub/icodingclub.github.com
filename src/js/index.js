7e74eedd0c638280b4002660f3aedee199cb77e4
icodingclub <mails.icodingclub@gmail.com>
2018-01-14 07:30:31 +0530

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,13 @@
 import articles from "./articles";
 import ArticleBuilder from "./ArticleBuilder";
 
-const articleBuilder = new ArticleBuilder({articles});
 
-document.getElementById("articlesContainer").innerHTML =articleBuilder.render();
 
-// $(document).ready(function() {
-//     setTimeout(function() {
-//         null == document.getElementById("tester") && $("#bannerMsg").slideDown("slow")
-//     }, 3e3)
-// });
+$(document).ready(function () {
+    const articleBuilder = new ArticleBuilder({ articles });
+    document.getElementById("articlesContainer").innerHTML = articleBuilder.render();
+
+    setTimeout(function () {
+        null == document.getElementById("tester") && $("#bannerMsg").slideDown("slow")
+    }, 3e3)
+});
