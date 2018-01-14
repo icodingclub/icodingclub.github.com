6d07059416b4589fe3d08a0a86c3fcadd15c5162
icodingclub <mails.icodingclub@gmail.com>
2018-01-15 02:24:56 +0530

    test

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,15 @@
 import articles from "./articles";
 import ArticleBuilder from "./ArticleBuilder";
-
+const articleBuilder = new ArticleBuilder({ articles });
+//window.articleBuilder = articleBuilder;
+//document.getElementById("articlesContainer").innerHTML = articleBuilder.render();
 
 
 $(document).ready(function () {
     const articleBuilder = new ArticleBuilder({ articles });
+    window.articleBuilder = articleBuilder;
     document.getElementById("articlesContainer").innerHTML = articleBuilder.render();
-
-    setTimeout(function () {
-        null == document.getElementById("tester") && $("#bannerMsg").slideDown("slow")
-    }, 3e3)
+    // setTimeout(function () {
+    //     null == document.getElementById("tester") && $("#bannerMsg").slideDown("slow")
+    // }, 3e3)
 });
