fc4e47e698ff129cfa6a6bc1c5a6141bba111d94
icodingclub <mails.icodingclub@gmail.com>
2018-01-27 09:57:09 +0530

    new article

---
+++ scripts/mdToHtml.js
@@ -0,0 +1,24 @@
+
+var prettify = require('showdown-prettify');
+var fs = require('fs');
+var showdown = require('showdown');
+
+function writeFile(file, data) {
+    console.log("File processing is over");
+    fs.writeFileSync(file, data, 'utf8');
+}
+
+const filePath = "object-iteration-es6/story";
+
+const inPath = `${__dirname}/../articles/${filePath}.md`;
+const outPath = `${__dirname}/../articles/${filePath}.html`;
+
+
+var contents = fs.readFileSync(inPath, 'utf8');
+var converter = new showdown.Converter({ extensions: ['prettify'] });
+converter.setFlavor('github');
+
+
+var html = converter.makeHtml(contents);
+
+writeFile(outPath, html);
