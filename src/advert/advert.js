5fee46f4f91698ba89993a04762141caafd1ebd5
Praveen Singh <praveenkumar_singh@intuit.com>
2020-11-27 21:38:51 +0530

    Jekyll Migration

--- src/advert/advert.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $("<div/>", {
-        id: "tester",
-        style: "display:none"
-    }).appendTo(document.body);
-});
--- src/js/index.js
+++
@@ -1,9 +0,0 @@
-import articles from "./articles";
-import ArticleBuilder from "./ArticleBuilder";
-import "../sass/main.scss";
-const articleBuilder = new ArticleBuilder({ articles });
-window.articleBuilder = articleBuilder;
-const articlesContainer =  document.getElementById("articlesContainer");
-if(articlesContainer){
-    articlesContainer.innerHTML = articleBuilder.render();
-}
